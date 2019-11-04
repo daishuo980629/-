0256b1055684554615076e6d8e5310165134a568
daishuo980629 <2813365670@qq.com>
2019-11-04 13:58:57 +0800

    第二周

---
+++ am/01.js
@@ -0,0 +1,16 @@
+// 版本控制系统
+//git =》github》gitab 分布式代码管理工具（多人共同管理项目）
+//svn 集成式代码管理工具 
+//svn：   两区： 工作区（本地）    服务区（线上）
+//git：   三区： 工作区    暂存区     服务区
+//github：  初始化：git  init 初始化
+//                 branch    分支
+                //    git status 查看文件状态
+                //    git add.    添加所有文件到暂存区  git add 文件  单个文件
+                //    git commit  -m 提交 并注释
+                //    git config --global user.name “用户名” 登录git
+                //    git config --global user.email “邮箱”
+                //    git remote add origin       连接仓库地址 
+                //    git push -u origin master    提交内容到master分支  git push
+                // git pull 多人合作先拉取
+                // git clone +仓库地址 克隆
